b3569f836f5a16c833e96f41ec7bb1df4a7290a8
Kavita Seshadri <kavitaseshadri@gmail.com>
2024-07-18 18:54:22 +0530

    18 july 2024 commit8

--- reactjsdemos/day27/bookapp1/src/components/BookShow.js
+++ reactjsdemos/day27/bookapp1/src/components/BookShow.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import BookEdit from './BookEdit';
 
-const BookShow = () => {
+const BookShow = ({ book, onDelete, onEdit }) => {
+    const [showEdit, setShowEdit] = useState(false);
+
+    const handleEditClick = () => {
+        setShowEdit(current => !current);
+    }
+    const handleDeleteClick = () => {
+        onDelete(book.id);
+    }
+
+    const handleSubmit = () => {
+        setShowEdit(false);
+        onEdit(id, newTitle);
+    }
+
+    let content = <h3>{book.title}</h3>
+    if (showEdit) {
+        content = <BookEdit onSubmit={handleSubmit}></BookEdit>
+    }
+
     return (
-        <div>BookShow</div>
+        <div>
+            <div>{content}</div>
+            <div>
+                <button type="button" onClick={handleEditClick}>Edit</button>
+                <button type="button" onClick={handleDeleteClick}>Delete</button>
+            </div>
+        </div>
     )
 }
 
