665b24c93942f1186de025a84a4b53ec84d19063
Kavita Seshadri <kavitaseshadri@gmail.com>
2024-07-18 18:45:48 +0530

    18 july 2024 commit6

--- reactjsdemos/day27/bookapp1/src/components/BookCreate.js
+++ reactjsdemos/day27/bookapp1/src/components/BookCreate.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const BookCreate = () => {
+
+    const [title, setTitle] = useState('');
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    }
+    const handleSubmit = (event) => {
+        console.log('handleSubmit called in BookCreate');
+        event.preventDefault();
+        //to prevent the form from being submitted to server side script
+        onCreate(title);
+        setTitle('');
+    }
+
     return (
-        <div>BookCreate</div>
+        <div>
+            <h3>Add a Book</h3>
+            <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input value={title} onChange={handleChange} />
+                <button type="submit">Create a Book</button>
+            </form>
+        </div>
     )
 }
 
