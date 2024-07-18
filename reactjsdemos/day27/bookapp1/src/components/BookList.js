2c5477e2497a96e8b96347b8d94bb481dc9a302d
Kavita Seshadri <kavitaseshadri@gmail.com>
2024-07-18 18:42:03 +0530

    18 july 2024 commit5

--- reactjsdemos/day27/bookapp1/src/components/BookList.js
+++ reactjsdemos/day27/bookapp1/src/components/BookList.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import BookShow from './BookShow'
 
-const BookList = () => {
+const BookList = ({ books, onDelete, onEdit }) => {
+    const renderedBooks = books.map((book) => {
+        return (
+            <BookShow
+                onEdit={onEdit}
+                onDelete={onDelete}
+                key={book.id}
+                book={book}
+            />
+        )
+    })
     return (
-        <div>BookList</div>
+        <div>{renderedBooks}</div>
     )
 }
 
